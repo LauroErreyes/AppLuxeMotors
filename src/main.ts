0cf622052883572fd15f236b3b99672fcfbc5538
LauroErreyes <laec0603@gmail.com>
2025-02-13 14:52:33 -0500

    Correcion cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { config } from 'dotenv';
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:4200'], // Permite Angular en desarrollo
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Permite cookies si usas autenticación con sesiones
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
