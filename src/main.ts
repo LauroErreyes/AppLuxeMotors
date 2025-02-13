846ee54d1a544222fed3a9c9e6bd959401247432
LauroErreyes <laec0603@gmail.com>
2025-02-13 15:01:50 -0500

    Acceso de cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
 
 async function bootstrap() {
-  config();
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200'], // Permite Angular en desarrollo
+    origin: "*", // Permite Angular en desarrollo
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, // Permite cookies si usas autenticación con sesiones
   });
   await app.listen(process.env.PORT ?? 3000);
 }
